aceef4e2d432d4fecbb92dfecd3336e52b349218
shadowstar2023 <imshadowstar@gmail.com>
2024-01-08 18:34:44 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 export NEZHA_SERVER="nezha.130136.xyz"
 export NEZHA_PORT="443"  #当端口设置为443时，自动开启TLS,无需设置
-export NEZHA_KEY="6Kf5QzDNLwaaPwSqxq"
+export NEZHA_KEY="uAj2i1ifPVqnrpKyjC"
 
 chmod +x server
 if [ "$NEZHA_PORT" = "443" ]; then
